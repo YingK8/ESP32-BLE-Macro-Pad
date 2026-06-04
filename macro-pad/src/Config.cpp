6b42fdfe48f620481176163f985a0bb2fbe22734
Kevin Ying <yingk@berkeley.edu>
2026-06-03 23:08:21 -0700

    buttons work again mostly

--- macro-pad/src/Config.cpp
+++ macro-pad/src/Config.cpp
@@ -4,7 +4,7 @@
 // .cpp files would otherwise create one copy per translation unit.
 
 const uint8_t ROW_PINS[ROWS] = {1, 5};
-const uint8_t COL_PINS[COLS] = {4, 20, 8};
+const uint8_t COL_PINS[COLS] = {4, 21, 8};
 
 const Phase PHASES[PHASE_COUNT] = {
     {"WORK",      30, true },
